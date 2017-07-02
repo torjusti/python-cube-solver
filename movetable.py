fd27a809483a2959222f5e7ed9ff8609f4c600d4
Torjus Iveland <torjusti@gmail.com>
2017-07-02 20:13:11 +0200

    Add MoveTable

---
+++ movetable.py
@@ -0,0 +1,22 @@
+import numpy
+
+class MoveTable:
+    def __init__(self, size, do_move):
+        self.create_table(size, do_move)
+
+    def create_table(self, size, do_move):
+        self.table = numpy.empty([18, size])
+
+        for i in range(size):
+            for move in range(18):
+                if move not in table[:, i]:
+                    result = do_move(i, move)
+                    self.table[i, move] = result
+                    inverse = move - 2 * (move % 3) + 2
+                    self.table[result, inverse] = i
+
+    def do_move(self, index, move):
+        return self.table[index, move]
+
+    def get_size():
+        return self.table.shape[1]
